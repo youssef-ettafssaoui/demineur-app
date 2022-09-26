d212c30dc0e4cac205eae8730af09c7cb5605e3d
youssef-ettafssaoui <yettafssaoui2@gmail.com>
2022-09-26 09:33:46 +0100

    Prettier & eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,17 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
+  extends: ["plugin:react/recommended", "standard-with-typescript"],
+  parser: "@typescript-eslint/parser",
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ["react", "@typescript-eslint"],
   rules: {
-  }
-}
+    quotes: ["error", "double"],
+  },
+};
